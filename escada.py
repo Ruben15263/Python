0523646df57e946bc0ea7a276c395918b5797bd3
Ruben15263 <133796664+Ruben15263@users.noreply.github.com>
2023-05-16 20:30:28 +0100

    Update escada.py

--- escada.py
+++ escada.py
@@ -3,7 +3,11 @@ t=turtle.Turtle()
 t.shape("turtle")
 ecra=turtle.getscreen()
 t.left(90)
-for i in range(5):
-    t.forward(50)
-    t.right()
-ecra.mainloop()
+for i in range(10):
+    if i % 2==0:
+        t.right(90)
+        t.forward(50)
+    if i % 2 !=0:
+        t.left(90)
+        t.forward(50)
+ecra.mainloop
